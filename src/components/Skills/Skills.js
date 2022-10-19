f258351b689325c28460548d8957b51b11408d8b
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-10-19 11:19:42 -0400

    Skills section format has been created. Images for each icon will still need to be added

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,15 +4,21 @@ import './skills.css'
 const Skills = () => {
   return (
     <div className="section">
+        <h1 className="section-title">Skills</h1>
         <div className="skills-container">
-            <div className="skills-title">
-                
-            </div>
-            <div className="skils-icons">
-                
+            <div className="skills-icons">
+                <div className="skill-icon">HTML</div>
+                <div className="skill-icon">Javascript</div>
+                <div className="skill-icon">React</div>
+                <div className="skill-icon">Redux</div>
+                <div className="skill-icon">CSS</div>
+                <div className="skill-icon">Python</div>
+                <div className="skill-icon">Git</div>
+                <div className="skill-icon">Django</div>
+                <div className="skill-icon">Webflow</div>
             </div>
             <div className="skills-details">
-                
+
             </div>
         </div>
     </div>
